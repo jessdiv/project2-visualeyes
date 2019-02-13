acde69a8cd5b28630263384ef942a56826f07e87
Declan Boller <declanjboller@outlook.com>
2019-02-13 14:11:13 +1100

    update js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 let menuButton = document.getElementById('hamburger');
-let sidebar = document.getElementById('sidebar')
+let sidebar = document.getElementById('sidebar');
 let dataBody = document.getElementById('data-main');
 let scrollHandler = document.getElementById('scrollHandler');
 
@@ -8,14 +8,14 @@ let toggleClass = function () {
   sidebar.classList.toggle('open');
   dataBody.style.marginLeft.toggle = '300px';
 
-  if(dataBody.style.marginLeft != '300px') {
+  if (dataBody.style.marginLeft != '300px') {
     dataBody.style.width = 'Calc(100vw - 75px)';
   }
 };
 
 let scrollDown = function () {
   window.scrollTo(0, 800);
-}
+};
 
 menuButton.addEventListener('click', toggleClass);
 scrollHandler.addEventListener('click', scrollDown);
@@ -23,91 +23,135 @@ scrollHandler.addEventListener('click', scrollDown);
 
 // ----------------------- Start Scatter Plot  ----------------------- \\
 
-/*
-*    main.js
-*    Mastering Data Visualization with D3.js
-*    Project 1 - Star Break Coffee
-*/
+let margin = { left: 80, right: 20, top: 50, bottom: 100 };
 
-var margin = { left:80, right:20, top:50, bottom:100 };
+let width = 800 - margin.left - margin.right;
+let height = 600 - margin.top - margin.bottom;
 
-var width = 1000 - margin.left - margin.right,
-    height = 800 - margin.top - margin.bottom;
+// Append Canvas to ds1
+var g = d3.select('#ds1')
+    .append('svg')
+        .attr('width', width + margin.left + margin.right)
+        .attr('height', height + margin.top + margin.bottom)
+    .append('g')
+        .attr('transform', 'translate(' + margin.left + ', ' + margin.top + ')');
 
-var g = d3.select("#ds1")
-    .append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-        .attr("transform", "translate(" + margin.left + ", " + margin.top + ")");
+var time = 0;
+
+// X Scale
+var x = d3.scaleLinear()
+    .range([0, width])
+    .domain([-5000, 69331]);
+
+// Y Scale
+var y = d3.scaleLinear()
+    .range([height, 0])
+    .domain([0, 100]);
+
+// Area
+var area = d3.scaleLinear()
+    .range([25 * Math.PI, 1500 * Math.PI])
+    .domain([0, 1500000000]);
+
+var continentColor = d3.scaleOrdinal().range(['#ffba49', '#20a39e', '#ef5b5b', '#912f56', '#f2e3bc', '#ff8552', '#f76f8e', '#14cc60', '#931621', '#b33951', '#40434e', '#d1f5ff', '#7d53de', '#e5446d', '#000']);
+
+// X Axis
+var xAxisCall = d3.axisBottom(x)
+    .tickValues([0, 5000, 10000, 15000, 20000, 25000, 30000, 35000, 40000, 45000, 50000, 55000, 60000, 65000])
+    .tickFormat(d3.format('$'))
+    .ticks(16);
+g.append('g')
+    .attr('class', 'x axis')
+    .attr('transform', 'translate(0,' + height + ')')
+    .call(xAxisCall);
+
+// Y Axis
+var yAxisCall = d3.axisLeft(y)
+    .tickFormat(function (d) { return +d; });
+g.append('g')
+    .attr('class', 'y axis')
+    .call(yAxisCall);
 
 // X Label
-g.append("text")
-    .attr("y", height + 50)
-    .attr("x", width / 2)
-    .attr("font-size", "20px")
+g.append('text')
+    .attr('y', height + 50)
+    .attr('x', width / 2)
+    .attr('font-size', '20px')
     .attr('color', 'black')
-    .attr("text-anchor", "middle")
-    .text("Country");
+    .attr('text-anchor', 'middle')
+    .text('GDP/Capita');
 
 // Y Label
-g.append("text")
-    .attr("y", -60)
-    .attr("x", -(height / 2))
-    .attr("font-size", "20px")
-    .attr("text-anchor", "middle")
-    .attr("transform", "rotate(-90)")
-    .text("Life Expectancy");
+g.append('text')
+    .attr('y', -60)
+    .attr('x', -(height / 2))
+    .attr('font-size', '20px')
+    .attr('text-anchor', 'middle')
+    .attr('transform', 'rotate(-90)')
+    .text('Life Expectancy');
 
-d3.csv("../resources/flat_data.csv").then(function(data){
-  let newArr = data.slice(0, 15);
-    console.log(newArr);
+// Time Label
+var timeLabel = g.append('text')
+    .attr('y', height - 10)
+    .attr('x', width - 40)
+    .attr('font-size', '40px')
+    .attr('opacity', '0.4')
+    .attr('text-anchor', 'middle')
+    .text('1960');
+
+// Load CSV Data
+d3.csv('../resources/flat_data.csv').then(function (data) {
+    const dataByYear = [];
+    for (let i = 0; i < data.length; i += 15) {
+      dataByYear.push(data.slice(i, i + 15));
+    }
+
+    console.log(dataByYear);
 
     // Clean data
-    newArr.forEach(function(d) {
+    dataByYear.forEach(function (year) {
+      year.forEach(function (d) {
         d['Life Expectancy'] = +d['Life Expectancy'];
+        d['Total Population'] = +d['Total Population'].replace(/,/g, '');
+        d['GPD/Capita'] = +d['GPD/Capita'].replace(/,/g, '');
+        d['GDP (Current USD)'] = +d['GDP (Current USD)'].replace(/,/g, '');
+      });
     });
 
-    // X Scale
-    var x = d3.scaleBand()
-        .domain(newArr.map(function(d){ return d.Country }))
-        .range([0, width])
-        .padding(0.2);
+    let update = function (data) {
+      console.log('update', data);
+      data.forEach(function (d) {
+        // console.log('iterating', d);
+        if (! d['Total Population']) return;
+        let t = d3.transition()
+            .duration(100);
 
-    // Y Scale
-    var y = d3.scaleLinear()
-        .domain([0, d3.max(data, function(d) { return d['Life Expectancy'] })])
-        .range([height, 0]);
+        let circles = g.selectAll('circle').data(data, function (d) {
+          return d.Country;
+        });
 
-    var area = d3.scaleLinear()
-        .range([15*Math.PI, 800*Math.PI])
-        .domain([0, 100]);
+        circles.exit()
+            .attr('class', 'exit')
+            .remove();
 
-    var continentColor = d3.scaleOrdinal().range(['#ffba49', '#20a39e', '#ef5b5b', '#912f56', '#f2e3bc', '#ff8552', '#f76f8e', '#14cc60', '#931621', '#b33951', '#40434e', '#d1f5ff', '#7d53de', '#e5446d', '#000']);
+        circles.enter()
+            .append('circle')
+            .attr('class', 'enter')
+            .attr('fill', function (d) { return continentColor(d['GPD/Capita']); })
+            .merge(circles)
+            .transition(t)
+                .attr('cy', function (d){ return y(d['Life Expectancy']); })
+                .attr('cx', function (d) { return x(d['GPD/Capita']); })
+                .attr('r', function (d){ return Math.sqrt(area(d['Total Population']) / Math.PI); });
 
-    // X Axis
-    var xAxisCall = d3.axisBottom(x);
-    g.append("g")
-        .attr("class", "x axis")
-        .attr("transform", "translate(0," + height +")")
-        .call(xAxisCall);
+        timeLabel.text(+(time + 1960));
+      });
 
-    // Y Axis
-    var yAxisCall = d3.axisLeft(y)
-        .tickFormat(function(d){ return d; });
-    g.append("g")
-        .attr("class", "y axis")
-        .call(yAxisCall);
-
-    // Bars
-    var circle = g.selectAll("circle")
-        .data(newArr)
-
-    circle.enter()
-        .append("circle")
-            .attr("cy", function(d){ return y(d['Life Expectancy']); })
-            .attr("cx", function(d){ return x(d.Country) + x.bandwidth() /2 })
-            .attr("r", function(d){ return Math.sqrt(area(d['Life Expectancy']) / Math.PI) })
-            .attr("fill", function(d) { return continentColor(d.Country); })
-
-})
+      setTimeout(function () {
+        console.log('setting timeout');
+        time = (time < 58) ? time + 1 : 0;
+        update(dataByYear[time]);
+      }, 100);
+    };
+    update(dataByYear[0]);
+  });
